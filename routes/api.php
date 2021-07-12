a846a3b8cb3f105accf77c93ecc370ae8624f091
thien <thien1988@gmail.com>
2021-07-12 14:47:31 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ComponentController;
+
+use App\Models\LandingPage;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +22,44 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('component', ComponentController::class);
+Route::post('/landing-page', function (Request $request) {
+    try {
+        $data = $request->json()->all();
+        
+        $landingPage = new LandingPage();
+        $landingPage->name = $data['name'];
+        $landingPage->description = $data['description'];
+        $landingPage->url = $data['url'];
+        $landingPage->title = $data['title'];
+        $landingPage->meta = $data['meta'];
+        $landingPage->components = json_encode ($data['components']);
+        $landingPage->save();
+
+
+    } catch(Exception $e) {
+        return 'fail';
+    }
+    return 'ok';
+});
+
+Route::get('/landing-page/{url}', function (Request $request) {
+    $landingPage = LandingPage::where('url', $request->url)->get();
+    
+    $landingPage[0]->components = json_decode($landingPage[0]->components, FALSE);
+    return $landingPage;
+});
+
+Route::post('/savefile', function (Request $request) {
+    $fileInfo = $request->fileInfo;
+    list($type, $name, $data) = explode(';', $fileInfo);
+
+    list(, $type) =  explode(':', $type);
+    list(, $name) = explode('=', $name);
+    list(, $data)      = explode(',', $data);
+    $data = base64_decode($data);
+
+    $path = public_path(). DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR;
+    $ok = file_put_contents($path.$name, $data);
+    return $ok;
+});
+
